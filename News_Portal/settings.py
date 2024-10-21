9ad1aca9b763154f4c43a800ff462baf591ee3df
VVither <kashirovb@gmail.com>
2024-10-21 16:28:22 +0400

    добавлено кэширование

--- News_Portal/settings.py
+++ News_Portal/settings.py
@@ -85,6 +85,13 @@ TEMPLATES = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
+    }
+}
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  # стандартный бэкенд
     'allauth.account.auth_backends.AuthenticationBackend',  # allauth
--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from .views import PostListView, PostSearchView, upgrade_me, profile_view
 from .views import NewsListView, NewsDetailView, NewsCreate, NewsUpdate, NewsDelete
 from .views import ArticlesCreate, ArticlesDetailView, ArticlesListView, ArticlesUpdate, ArticlesDelete
 from .views import CategoryListView, CategoryDetailView, subscribe_to_category
+from django.views.decorators.cache import cache_page
 
 app_name = 'news'
 
@@ -11,14 +12,14 @@ urlpatterns = [
     # path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', profile_view, name='profile_view'),
     path('upgrade/', upgrade_me, name= 'upgrade'),
-    path('post/', PostListView.as_view(), name='post_list'), # Общий список
-    path('post/news/', NewsListView.as_view(), name='news_list'), # Cписок новостей
+    path('post/', cache_page(60*5)(PostListView.as_view()), name='post_list'), # Общий список
+    path('post/news/', cache_page(60*5)(NewsListView.as_view()), name='news_list'), # Cписок новостей
     path('post/news/<int:pk>/', NewsDetailView.as_view(), name='news_detail'), # Детализация новости
     path('post/news/search/', PostSearchView.as_view(), name='search'), # Поиск
     path('post/news/create/', NewsCreate.as_view(), name='news_create'), # Создание новостей
     path('post/news/<int:pk>/update/', NewsUpdate.as_view(), name='news_update'), # Редактирование новостей
     path('post/news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'), # Подтверждение удаления новости
-    path('post/articles/', ArticlesListView.as_view(), name='articles_list'), # Список статей
+    path('post/articles/', cache_page(60*5)(ArticlesListView.as_view()), name='articles_list'), # Список статей
     path('post/articles/<int:pk>/', ArticlesDetailView.as_view(), name='articles_detail'), # Детализация статьи
     path('post/articles/create', ArticlesCreate.as_view(), name='articles_create'), # Создание статьи
     path('post/articles/<int:pk>/update/', ArticlesUpdate.as_view(), name='articles_update'), # Редактирование статьи
